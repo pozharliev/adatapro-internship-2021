809802a610718336eafe8d3a49c4ed24d919809e
Atanas Pozharliev <ABPozharliev19@codingburgas.bg>
2021-07-17 21:19:35 +0300

    Implement models for the Profile

--- api/signUpView/models.py
+++ api/signUpView/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 
 class Profile(models.Model):
-    pass
+    profile_id = models.BigAutoField(primary_key=True)
+    profile_username = models.CharField(max_length=64)
+    profile_email = models.EmailField(max_length=128, unique=True)
+    password_hash = models.CharField(max_length=256)
+
+    class Meta:
+        managed = True
