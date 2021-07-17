e67828202ef2062c4fdc9c9f9ef86e09207ad1ec
Atanas Pozharliev <ABPozharliev19@codingburgas.bg>
2021-07-17 15:19:37 +0300

    Add default route and connect index.html to it

--- api/settings.py
+++ api/settings.py
@@ -45,7 +45,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': ['api/signUpView/',
-                 'api/logInView/'
+                 'api/logInView/',
+                 'api/homeView/'
                  ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -109,7 +110,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "api/static/"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ from .signUpView import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', include('api.signUpView.urls')),
-    path('login/', include('api.logInView.urls'))
+    path('login/', include('api.logInView.urls')),
+    path('', include('api.homeView.urls'))
 ]
