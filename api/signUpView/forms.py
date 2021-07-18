a08d0ccef037b8cf30da695023908a4f28822572
KNTaligadzhiev19 <KNTaligadzhiev19@codingburgas.bg>
2021-07-18 12:13:29 +0300

    Modify Signup Form

--- api/signUpView/forms.py
+++ api/signUpView/forms.py
@@ -4,13 +4,13 @@ from django import forms
 
 
 class SignUpForm(ModelForm):
-    profile_username = forms.CharField(label="Username", max_length=64, widget=forms.TextInput(attrs={'class': 'input',
+    profile_username = forms.CharField(label="Username", max_length=64, widget=forms.TextInput(attrs={'class': 'input mr',
                                                                                                       'placeholder': 'Mitko123 '
                                                                                                       }))
-    profile_email = forms.EmailField(label="Email", max_length=128, widget=forms.EmailInput(attrs={'class': 'input',
+    profile_email = forms.EmailField(label="Email", max_length=128, widget=forms.EmailInput(attrs={'class': 'input mr',
                                                                                                    'placeholder': 'mitko123@adatapro.bg '
                                                                                                    }))
-    password_hash = forms.CharField(label='Password', max_length=256, widget=forms.PasswordInput({'class': 'input',
+    password_hash = forms.CharField(label='Password', max_length=256, widget=forms.PasswordInput({'class': 'input mr',
                                                                                                   'placeholder': '********',
                                                                                                   }))
 
