df2c3b30c339078a47a3d439e8faf75afecef6a1
Atanas Pozharliev <ABPozharliev19@codingburgas.bg>
2021-07-19 10:50:08 +0300

    Modify return in register menu So that if the register is successful it returns 'successful-register' And if the register is not successful it returns 'unsuccessful-register''

--- api/signUpView/views.py
+++ api/signUpView/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import FormView
 from .forms import SignUpForm
 from api.helpers.validations import validation
+from django.http import JsonResponse
 
 
 class SignUp(FormView):
@@ -10,13 +11,12 @@ class SignUp(FormView):
     success_url = '/'
 
     def post(self, request, *args, **kwargs):
-        login = super(SignUp, self).post(self, request, *args, **kwargs)
         username = request.POST['profile_username']
         email = request.POST['profile_email']
         password = request.POST['password_hash']
         login_info = SignUpForm(request.POST)
         if validation(username, email, password) and login_info.is_valid():
             login_info.save(commit=True)
-            return login
+            return JsonResponse({'type': 'successful-register'})
         else:
-            return redirect('/')
+            return JsonResponse({'type': 'unsuccessful-register'})
