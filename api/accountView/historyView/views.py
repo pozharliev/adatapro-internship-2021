3f5c1e06a4dc800c0eec3adb73569f16ebc14f52
Atanas Pozharliev <ABPozharliev19@codingburgas.bg>
2021-07-25 14:28:01 +0300

    Finish implementation of preference of data

--- api/accountView/historyView/views.py
+++ api/accountView/historyView/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import ListView
 from api.accountView.historyView.models import HistoryScraping
 from api.accountView.models import ProfilePreferences
+import operator
+from functools import reduce
+from django.db.models import Q
 
 
 class HistoryView(ListView):
@@ -8,29 +11,15 @@ class HistoryView(ListView):
     model = HistoryScraping
     paginate_by = 5
 
-    def get_queryset(self,):
-        query = super(HistoryView, self).get_queryset()
-        preferences = {
-            'Ardes': None,
-            'Ozone': None,
-            'Laptop': None,
-            'Polycomp': None,
-            'Jar': None,
-            'Also': None,
-            'Emag': None,
-            'Plesio': None
-        }
-        for p in ProfilePreferences.objects.raw(f"""
-        SELECT id, want_laptop, want_polycomp, want_jar, want_also, want_ardes, want_ozone, want_emag, want_plesio
-        FROM accountview_profilepreferences
-        WHERE profile_username_id = {self.request.user.id}
-        """):
-            preferences['Ardes'] = p.want_ardes
-            preferences['Ozone'] = p.want_ozone
-            preferences['Laptop'] = p.want_laptop
-            preferences['Polycomp'] = p.want_polycomp
-            preferences['Jar'] = p.want_jar
-            preferences['Also'] = p.want_also
-            preferences['Emag'] = p.want_emag
-            preferences['Plesio'] = p.want_plesio
-        return query
+    @property
+    def get_filter_terms(self):
+        profile_preferences = ProfilePreferences.objects.get(profile_username=self.request.user)
+        site_names = [k.split('_')[-1] for k, v in profile_preferences.__dict__.items() if v and k.startswith('want')]
+        filter_terms = [Q(**{'site__icontains': site_name}) for site_name in site_names]
+
+        return filter_terms
+
+    def get_queryset(self, ):
+        queryset = super(HistoryView, self).get_queryset()
+
+        return queryset.filter(reduce(operator.or_, self.get_filter_terms))
