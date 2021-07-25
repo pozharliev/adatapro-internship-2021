6a7e2ad4639bcbdb0f937de449ba188f4689f737
Ludfik <venito1234@abv.bg>
2021-07-25 16:37:32 +0300

    Laptop and Ardes sites crawlers added + Main start crawling file

---
+++ crawlers/crawlers/spiders/CrawlerRunner.py
@@ -0,0 +1,10 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+process = CrawlerProcess(get_project_settings())
+
+process.crawl('laptop')
+process.crawl('ardes')
+process.crawl('ozone')
+
+process.start()
