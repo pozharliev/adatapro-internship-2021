a6a67c77c33d1bc132f05528baa53fc1f4541f2e
Atanas Pozharliev <ABPozharliev19@codingburgas.bg>
2021-07-25 23:15:24 +0300

    Add regex for validators

--- api/helpers/validations.py
+++ api/helpers/validations.py
@@ -1,2 +1,12 @@
+import re
+
+
 def validation(username, email, password):
-    return True
+    username_regex = '/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/'
+    email_regex = '/(([a-z]+)([._a-z0-9])([a-z0-9]+)).{1,64}(@)([a-z]+)([.a-z])([a-z])+/gmi'
+    password_regex = "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+    if re.match(username_regex, username) and re.match(email_regex, email) and re.match(password_regex, password):
+        return True
+
+    else:
+        return False
